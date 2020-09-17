d174a6fa9bf1954e759aacd6e5ceab17a49dd28b
122163@gdpr.com <andychan>
2020-09-17 11:11:40 +0800

    +组织变更记录

---
+++ src/main/java/net/herdao/hdp/mpclient/service/OrgModifyRecordService.java
@@ -0,0 +1,15 @@
+
+package net.herdao.hdp.mpclient.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import net.herdao.hdp.mpclient.entity.OrgModifyRecord;
+
+/**
+ * 
+ *
+ * @author Andy
+ * @date 2020-09-17 11:03:56
+ */
+public interface OrgModifyRecordService extends IService<OrgModifyRecord> {
+
+}
---
+++ src/main/java/net/herdao/hdp/mpclient/service/impl/OrgModifyRecordServiceImpl.java
@@ -0,0 +1,19 @@
+
+package net.herdao.hdp.mpclient.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import net.herdao.hdp.mpclient.entity.OrgModifyRecord;
+import net.herdao.hdp.mpclient.mapper.OrgModifyRecordMapper;
+import net.herdao.hdp.mpclient.service.OrgModifyRecordService;
+import org.springframework.stereotype.Service;
+
+/**
+ * 
+ *
+ * @author Andy
+ * @date 2020-09-17 11:03:56
+ */
+@Service
+public class OrgModifyRecordServiceImpl extends ServiceImpl<OrgModifyRecordMapper, OrgModifyRecord> implements OrgModifyRecordService {
+
+}
