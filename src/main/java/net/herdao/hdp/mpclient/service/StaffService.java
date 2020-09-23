f2dafbc011b33a778dd6de8481caae1e47abdc62
yangrr <121748@gdpr.com>
2020-09-23 20:23:11 +0800

    员工表接口

---
+++ src/main/java/net/herdao/hdp/mpclient/service/StaffService.java
@@ -0,0 +1,31 @@
+/*
+ *    Copyright (c) 2018-2025, hdp All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * Neither the name of the pig4cloud.com developer nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * Author: hdp
+ */
+
+package net.herdao.hdp.mpclient.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import net.herdao.hdp.mpclient.entity.Staff;
+
+/**
+ * 员工表
+ *
+ * @author yangrr
+ * @date 2020-09-23 18:10:29
+ */
+public interface StaffService extends IService<Staff> {
+
+}
