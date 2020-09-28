19a1674ad0ce771a3608ac614bb587ff94524d0e
yangrr <121748@gdpr.com>
2020-09-28 17:27:07 +0800

    劳资情况

--- src/main/java/net/herdao/hdp/manpower/mpclient/controller/StaffController.java
+++ src/main/java/net/herdao/hdp/manpower/mpclient/controller/StaffController.java
@@ -17,8 +17,12 @@
 
 package net.herdao.hdp.manpower.mpclient.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import net.herdao.hdp.manpower.mpclient.entity.*;
+import net.herdao.hdp.manpower.mpclient.service.*;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -40,14 +44,6 @@ import lombok.AllArgsConstructor;
 import net.herdao.hdp.common.core.util.R;
 import net.herdao.hdp.common.log.annotation.SysLog;
 import net.herdao.hdp.manpower.mpclient.dto.StaffHomePage;
-import net.herdao.hdp.manpower.mpclient.entity.Familystatus;
-import net.herdao.hdp.manpower.mpclient.entity.Staff;
-import net.herdao.hdp.manpower.mpclient.entity.Userposthistory;
-import net.herdao.hdp.manpower.mpclient.entity.Workexperience;
-import net.herdao.hdp.manpower.mpclient.service.FamilystatusService;
-import net.herdao.hdp.manpower.mpclient.service.StaffService;
-import net.herdao.hdp.manpower.mpclient.service.UserposthistoryService;
-import net.herdao.hdp.manpower.mpclient.service.WorkexperienceService;
 
 
 /**
@@ -69,6 +65,8 @@ public class StaffController {
     private final FamilystatusService familystatusService;
 
     private final UserposthistoryService userposthistoryService;
+
+    private final StaffcontractService staffcontractService;
 
     /**
      * 分页查询
@@ -138,6 +136,26 @@ public class StaffController {
         );
         StaffHomePage entity = new StaffHomePage(staff, null,null, familyList);
         return R.ok(entity);
+    }
+
+    /**
+     * 通过id查询员工劳资情况
+     * @param id id
+     * @return R
+     */
+    @ApiOperation(value = "通过id查询员工劳资情况", notes = "通过id查询")
+    @GetMapping("/staffwelfare/{id}" )
+//    @PreAuthorize("@pms.hasPermission('mpclient_staff_view')" )
+    public R getStaffWelfare(@PathVariable("id" ) Long id) {
+        Staff staff = staffService.getById(id);
+        List<Staffcontract> contractList = staffcontractService.list(new QueryWrapper<Staffcontract>()
+                .eq("SATFF_ID", staff.getId())
+                .orderByDesc("MODIFIED_TIME")
+        );
+        Map<String, Object> map = new HashMap<>();
+        map.put("staff", staff);
+        map.put("contractList", contractList);
+        return R.ok(map);
     }
 
     /**
