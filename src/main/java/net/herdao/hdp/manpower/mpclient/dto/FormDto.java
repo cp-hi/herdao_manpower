3615a5165a65b72e10847f1cd782c26cc1e48ed5
122163@gdpr.com <andychan>
2020-09-29 18:38:36 +0800

    +页面接口

---
+++ src/main/java/net/herdao/hdp/manpower/mpclient/dto/FormDto.java
@@ -0,0 +1,26 @@
+package net.herdao.hdp.manpower.mpclient.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+@Data
+@ApiModel(value = "页面DTO")
+public class FormDto {
+
+    @ApiModelProperty(value="名称")
+    private String name;
+
+    @ApiModelProperty(value="备注")
+    private String remark;
+
+    @ApiModelProperty(value="操作")
+    private Map<String,Object> operateMap;
+
+
+}
