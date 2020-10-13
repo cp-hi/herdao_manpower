6d7a33c7bf24302d96be6ba86b0a98c06d10bb87
yangrr <121748@gdpr.com>
2020-10-13 11:31:19 +0800

    集团模版化

---
+++ src/main/java/net/herdao/hdp/manpower/mpclient/dto/GroupFormDto.java
@@ -0,0 +1,28 @@
+package net.herdao.hdp.manpower.mpclient.dto;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+@Data
+@ApiModel(value = "集团表单")
+public class GroupFormDto {
+
+    private Long id;
+
+    @ApiModelProperty(value="集团名称")
+    private String groupName;
+
+    @ApiModelProperty(value="集团编码")
+    private String groupCode;
+
+    @ApiModelProperty(value="对应组织")
+    private String orgCode;
+
+    @ApiModelProperty(value="排序")
+    private Integer sortNo;
+
+    @ApiModelProperty(value="备注")
+    private String remark;
+
+}
