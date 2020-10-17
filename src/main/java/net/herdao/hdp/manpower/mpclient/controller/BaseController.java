067f5e413aacf7b6ba30fbfda7f4b4b7b334848b
122092@gdpr.com <122092@gdpr.com>
2020-10-17 14:56:24 +0800

    去除一个泛型参数

--- src/main/java/net/herdao/hdp/manpower/mpclient/controller/BaseController.java
+++ src/main/java/net/herdao/hdp/manpower/mpclient/controller/BaseController.java
@@ -12,6 +12,7 @@ import net.herdao.hdp.manpower.mpclient.service.EntityService;
 import net.herdao.hdp.manpower.mpclient.utils.ExcelUtils;
 import net.herdao.hdp.manpower.sys.service.OperationLogService;
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.NotImplementedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -30,12 +31,21 @@ import java.lang.reflect.ParameterizedType;
  * @Version 1.0
  */
 //TODO 实现导出功能
-public class BaseController<T, D extends T> {
+public class BaseController<T> {
 
     EntityService entityService;
 
     @Autowired
     OperationLogService operationLogService;
+
+    /**
+     * 批量导入、修改所用的类
+     * @return
+     * @Author ljan
+     */
+    protected Class getImportClass() {
+        throw new NotImplementedException("如果需要使用批量导入功能，请继承此方法");
+    }
 
     @ApiOperation(value = "获取操作记录")
     @GetMapping("/operationLog/{objId}")
@@ -46,7 +56,6 @@ public class BaseController<T, D extends T> {
         Class<T> clazz = (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
         return R.ok(operationLogService.findByEntity(objId, clazz.getName()));
     }
-
 
 //    @ApiResponses({
 //            @ApiResponse()
@@ -59,6 +68,7 @@ public class BaseController<T, D extends T> {
     public R getById(@PathVariable("id") Long id) {
         return R.ok(entityService.getById(id));
     }
+
     @PostMapping
     public R save(@RequestBody T t) {
         entityService.saveEntity(t);
@@ -92,16 +102,16 @@ public class BaseController<T, D extends T> {
             @ApiImplicitParam(name = "type", value = "操作类型，0:批量新增 1:批量修改"),
     })
     public R importData(HttpServletResponse response, @RequestParam(value = "file") MultipartFile file, Integer importType) throws Exception {
-        Class<D> clazz = (Class<D>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[1];
+//        Class<D> clazz = (Class<D>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[1];
         ImportExcelListener listener = null;
         InputStream inputStream = null;
         try {
             inputStream = file.getInputStream();
             listener = new ImportExcelListener(entityService, importType);
-            EasyExcel.read(inputStream, clazz, listener).sheet().doRead();
+            EasyExcel.read(inputStream, getImportClass(), listener).sheet().doRead();
             return R.ok(" easyexcel读取上传文件成功");
         } catch (Exception ex) {
-            ExcelUtils.export2Web(response, "职级导入错误信息", "职级导入错误信息", clazz, listener.getDataList());
+            ExcelUtils.export2Web(response, "职级导入错误信息", "职级导入错误信息", getImportClass(), listener.getDataList());
             return R.failed(ex.getMessage());
         } finally {
             IOUtils.closeQuietly(inputStream);
