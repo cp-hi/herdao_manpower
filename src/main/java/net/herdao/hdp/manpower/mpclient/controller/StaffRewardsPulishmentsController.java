0d76b623caa4c6135b4666c269804656f5b7a3ae
122163@gdpr.com <andychan>
2020-10-18 17:57:39 +0800

    *修改员工奖惩

--- src/main/java/net/herdao/hdp/manpower/mpclient/controller/StaffRewardsPulishmentsController.java
+++ src/main/java/net/herdao/hdp/manpower/mpclient/controller/StaffRewardsPulishmentsController.java
@@ -1,19 +1,4 @@
-/*
- *    Copyright (c) 2018-2025, hdp All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice,
- * this list of conditions and the following disclaimer.
- * Redistributions in binary form must reproduce the above copyright
- * notice, this list of conditions and the following disclaimer in the
- * documentation and/or other materials provided with the distribution.
- * Neither the name of the pig4cloud.com developer nor the names of its
- * contributors may be used to endorse or promote products derived from
- * this software without specific prior written permission.
- * Author: hdp
- */
+
 
 package net.herdao.hdp.manpower.mpclient.controller;
 
@@ -131,12 +116,12 @@ public class StaffRewardsPulishmentsController extends BaseController<StaffRewar
             EasyExcel.read(inputStream, StaffRpVO.class, listener).sheet().doRead();
             IOUtils.closeQuietly(inputStream);
 
-           /* return R.ok(" easyexcel读取上传文件成功");*/
+
         } catch (Exception ex) {
             ExcelUtils.export2Web(response, "员工奖惩错误信息", "员工奖惩错误信息", StaffRpVO.class, listener.getDataList());
-            /*return R.failed(ex.getMessage());*/
+            return R.failed(ex.getMessage());
         }
 
-        return null;
-    }
+        return R.ok("easyexcel读取上传文件成功");
+     }
 }
