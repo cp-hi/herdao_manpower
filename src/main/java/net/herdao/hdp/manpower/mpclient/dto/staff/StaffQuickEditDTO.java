e5fe294c39175b4ce3a3b422389bef993f18b1fd
yangrr <121748@gdpr.com>
2020-10-19 18:44:24 +0800

    新增

--- src/main/java/net/herdao/hdp/manpower/mpclient/dto/staff/StaffQuickEditDTO.java
+++ src/main/java/net/herdao/hdp/manpower/mpclient/dto/staff/StaffQuickEditDTO.java
@@ -1,10 +1,12 @@
 package net.herdao.hdp.manpower.mpclient.dto.staff;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 /**
  * @author yangrr
@@ -27,6 +29,8 @@ public class StaffQuickEditDTO {
     @ApiModelProperty(value="身份证号码")
     private String idNumber;
 
+    @JsonFormat(pattern = "yyyy/MM/dd", timezone = "GMT+8")
+    @DateTimeFormat(pattern = "yyyy/MM/dd")
     @ApiModelProperty(value="入职本公司日期")
-    private LocalDateTime entryTime;
+    private LocalDate entryTime;
 }
