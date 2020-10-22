b4976cfc564f78c5e40c8b3d8ab5a9ae8e7b30b2
122092@gdpr.com <122092@gdpr.com>
2020-10-22 08:44:32 +0800

    一键创建职级相关实体类

---
+++ src/main/java/net/herdao/hdp/manpower/mpclient/entity/OKJobLevel.java
@@ -0,0 +1,16 @@
+package net.herdao.hdp.manpower.mpclient.entity;
+
+import lombok.Data;
+
+/**
+ * @ClassName OKJobLevel
+ * @Description  OK = one key 一键职级
+ * @Author ljan
+ * @mail 122092@gdpr.com
+ * @Date 2020/10/21 19:24
+ * @Version 1.0
+ */
+@Data
+public class OKJobLevel extends JobLevel {
+    private Long ok_job_grade_id;
+}
