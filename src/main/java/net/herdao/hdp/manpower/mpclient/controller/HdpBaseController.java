ff0fd83e9024bf26d88ad9336f5e14020fb2ad05
liuxm <121786@gdpr.com>
2020-10-29 19:50:14 +0800

    更新下载模板参数接收问题

--- src/main/java/net/herdao/hdp/manpower/mpclient/controller/HdpBaseController.java
+++ src/main/java/net/herdao/hdp/manpower/mpclient/controller/HdpBaseController.java
@@ -9,6 +9,7 @@ import java.util.stream.Collectors;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.alibaba.excel.EasyExcelFactory;
 import com.alibaba.fastjson.JSON;
@@ -224,7 +225,7 @@ public abstract class HdpBaseController {
 	@ApiOperation(value = "下载批量新增、编辑模板")
 	@PostMapping("/downloadTemplate")
     @ApiImplicitParam(name = "importType", value = "下载模板类型，值： 0  批量新增模板； 值 1 批量修改模板")
-	public R downloadTemplate(HttpServletResponse response, ExportDataVO exportDataVO) {
+	public R downloadTemplate(HttpServletResponse response, @RequestBody ExportDataVO exportDataVO) {
 		int importType = exportDataVO.getImportType();
     	// 导出处理 class
 		Class excelCls = getExcelCls(importType);
