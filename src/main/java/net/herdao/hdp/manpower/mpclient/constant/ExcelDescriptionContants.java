665df0bb3f351bb07f060302c2626fa9eee7337d
122163@gdpr.com <andychan>
2020-10-29 14:01:16 +0800

    *修改合同测试bug

--- src/main/java/net/herdao/hdp/manpower/mpclient/constant/ExcelDescriptionContants.java
+++ src/main/java/net/herdao/hdp/manpower/mpclient/constant/ExcelDescriptionContants.java
@@ -29,4 +29,46 @@ public class ExcelDescriptionContants {
 			    .append("5、组织负责人工号请输入在职员工的工号；负责岗位请输入系统中已启用的岗位");
 		return eds.toString();
 	}
+
+	/**
+	 * 获取批量工作经历新增说明
+	 * @return
+	 */
+	public static String getWorkAddDesc() {
+		StringBuffer eds = new StringBuffer();
+		eds.append("导入说明：")
+				.append(LINE_FEED)
+				.append("1、标红字段为必填。")
+				.append(LINE_FEED)
+				.append("2、操作导入前请删除示例数据。")
+				.append(LINE_FEED)
+				.append("3、员工工号，员工姓名：请输入系统中已存在的员工工号和员工姓名，员工工号和员工姓名必须匹配一致。")
+				.append(LINE_FEED)
+				.append("4、下属人数：正整数。")
+				.append(LINE_FEED)
+				.append("5、开始日期、结束日期：格式为yyyy/MM/dd 或者 yyyy-MM-dd 或者 yyyy.MM.dd。结束时间必须在开始时间之后。");
+		return eds.toString();
+	}
+
+	/**
+	 * 获取批量工作经历编辑说明
+	 * @return
+	 */
+	public static String getWorkUpdateDesc() {
+		StringBuffer eds = new StringBuffer();
+		eds.append("导入说明：")
+				.append(LINE_FEED)
+				.append("1、标红字段为必填。")
+				.append(LINE_FEED)
+				.append("2、操作导入前请删除示例数据。")
+				.append(LINE_FEED)
+				.append("3、员工工号，员工姓名：请输入系统中已存在的员工工号和员工姓名，员工工号和员工姓名必须匹配一致。")
+				.append(LINE_FEED)
+				.append("4、下属人数：正整数。")
+				.append(LINE_FEED)
+				.append("5、开始日期、结束日期：格式为yyyy/MM/dd 或者 yyyy-MM-dd 或者 yyyy.MM.dd。结束时间必须在开始时间之后。")
+				.append(LINE_FEED)
+				.append("6、员工姓名+员工工号+开始时间+结束时间+单位名称:数据唯一标识，不允许重复导入记录。");
+		return eds.toString();
+	}
 }
