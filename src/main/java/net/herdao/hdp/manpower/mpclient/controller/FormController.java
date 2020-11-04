c4fcce130e31fcb774a230f6fdc9549794ad0ce3
122163@gdpr.com <andychan>
2020-11-04 11:53:23 +0800

    *修改接口 整合HdpBaseController

--- src/main/java/net/herdao/hdp/manpower/mpclient/controller/FormController.java
+++ src/main/java/net/herdao/hdp/manpower/mpclient/controller/FormController.java
@@ -1,19 +1,4 @@
-/*
- *    Copyright (c) 2018-2025, hdp All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice,
- * this list of conditions and the following disclaimer.
- * Redistributions in binary form must reproduce the above copyright
- * notice, this list of conditions and the following disclaimer in the
- * documentation and/or other materials provided with the distribution.
- * Neither the name of the pig4cloud.com developer nor the names of its
- * contributors may be used to endorse or promote products derived from
- * this software without specific prior written permission.
- * Author: hdp
- */
+
 
 package net.herdao.hdp.manpower.mpclient.controller;
 
@@ -73,7 +58,7 @@ public class FormController {
 
             FormDTO formDTO3 = new FormDTO();
             formDTO3.setId("3");
-            formDTO3.setName("织职级统计表");
+            formDTO3.setName("职级统计表");
             Map<String,Object> operateMap3=new HashMap<>();
             operateMap3.put("download","/orgReport/exportOrgJobLevel");
             operateMap3.put("preview","/orgReport/fetchOrgJobLevelView");
