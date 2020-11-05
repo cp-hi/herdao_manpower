e7b79b99e8aec206a3bf99b012e4025ef8f1034e
122092@gdpr.com <122092@gdpr.com>
2020-11-05 11:31:14 +0800

    调整操作记录时间格式

--- src/main/java/net/herdao/hdp/manpower/sys/entity/OperationLog.java
+++ src/main/java/net/herdao/hdp/manpower/sys/entity/OperationLog.java
@@ -3,6 +3,7 @@ package net.herdao.hdp.manpower.sys.entity;
 import com.alibaba.excel.annotation.ExcelIgnore;
 import com.alibaba.excel.annotation.ExcelIgnoreUnannotated;
 import com.alibaba.excel.annotation.ExcelProperty;
+import com.alibaba.excel.annotation.format.DateTimeFormat;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
@@ -32,36 +33,37 @@ public class OperationLog {
     @TableId(value = "ID", type = IdType.AUTO)
     private Integer id;
 
-    @ExcelProperty(value = {"","操作"})
-    @ApiModelProperty(value="操作")
+    @ExcelProperty(value = {"", "操作"})
+    @ApiModelProperty(value = "操作")
     private String operation;
 
-    @ExcelProperty(value =  {"","操作内容"})
-    @ApiModelProperty(value="操作内容")
+    @ExcelProperty(value = {"", "操作内容"})
+    @ApiModelProperty(value = "操作内容")
     private String content;
 
-    @ApiModelProperty(value="操作者ID")
+    @ApiModelProperty(value = "操作者ID")
     private Long operatorId;
 
-    @ExcelProperty(value =  {"","操作者名称"})
-    @ApiModelProperty(value="操作者名称")
+    @ExcelProperty(value = {"", "操作者名称"})
+    @ApiModelProperty(value = "操作者名称")
     private String operator;
 
-    @ExcelProperty(value =  {"","操作时间"})
-    @ApiModelProperty(value="操作时间")
+    @ExcelProperty(value = {"", "操作时间"})
+    @ApiModelProperty(value = "操作时间")
     @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
+    @DateTimeFormat("yyyy-MM-dd")
     private Date operatedTime;
 
-    @ApiModelProperty(value="被操作的实体类",hidden = true)
+    @ApiModelProperty(value = "被操作的实体类", hidden = true)
     private String entityClass;
 
-    @ApiModelProperty(value="被操作的实体类主键",hidden = true)
+    @ApiModelProperty(value = "被操作的实体类主键", hidden = true)
     private Long objId;
 
-    @ApiModelProperty(value="额外信息",hidden = true)
+    @ApiModelProperty(value = "额外信息", hidden = true)
     private String extraKey;
 
-    @ApiModelProperty(value="模块名",hidden = true)
+    @ApiModelProperty(value = "模块名", hidden = true)
     private String module;
 
 }
