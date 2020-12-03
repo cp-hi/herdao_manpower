efb06ac152d3745c1a9164aff413cf3483915705
122163@gdpr.com <andychan>
2020-12-03 15:14:09 +0800

    +入职登记记录-导出Excel

--- src/main/java/net/herdao/hdp/manpower/mpclient/controller/EntryJobController.java
+++ src/main/java/net/herdao/hdp/manpower/mpclient/controller/EntryJobController.java
@@ -143,10 +143,37 @@ public class EntryJobController {
         @ApiImplicitParam(name="page",value="分页对象",required = true),
         @ApiImplicitParam(name="orgId",value="组织ID"),
         @ApiImplicitParam(name="entryCheckStatus",value="入职登记记录 (1:已提交，2：已提交，3：已确认）"),
-        @ApiImplicitParam(name="searchText",value="关键字搜索"),
+        @ApiImplicitParam(name="searchText",value="关键字搜索")
     })
     public R<Page<EntryRegisterDTO>> findEntryRegisterPage(Page<EntryRegisterDTO> page, String orgId, String entryCheckStatus, String searchText) {
         Page<EntryRegisterDTO> pageResult = staffEntrypostApproveService.findEntryRegisterPage(page, orgId, entryCheckStatus, searchText);
         return R.ok(pageResult);
     }
+
+    /**
+     * 入职登记记录-导出Excel
+     * @param response
+     * @param orgId 组织ID
+     * @param entryCheckStatus 入职登记记录 (1:已提交，2：已提交，3：已确认）
+     * @param searchText 关键字搜索
+     * @return R
+     */
+    @ApiOperation(value = "入职登记记录-导出Excel", notes = "入职登记记录-导出Excel")
+    @GetMapping("/exportEntryRegisterPage")
+    @ApiImplicitParams({
+        @ApiImplicitParam(name="page",value="分页对象",required = true),
+        @ApiImplicitParam(name="orgId",value="组织ID"),
+        @ApiImplicitParam(name="entryCheckStatus",value="入职登记记录 (1:已提交，2：已提交，3：已确认）"),
+        @ApiImplicitParam(name="searchText",value="关键字搜索")
+    })
+    @SneakyThrows
+    public R<EntryRegisterDTO> exportEntryRegisterPage(HttpServletResponse response,  String orgId, String entryCheckStatus, String searchText) {
+        Page page = new Page();
+        page.setSize(-1);
+        Page<EntryRegisterDTO> pageResult = staffEntrypostApproveService.findEntryRegisterPage(page, orgId, entryCheckStatus, searchText);
+        ExcelUtils.export2Web(response, "入职登记记录", "入职登记记录", EntryRegisterDTO.class, pageResult.getRecords());
+        EntryRegisterDTO dto=new EntryRegisterDTO();
+        return R.ok(dto);
+    }
+
 }
