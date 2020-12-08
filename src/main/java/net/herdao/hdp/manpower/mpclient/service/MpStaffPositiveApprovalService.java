41af22eb8a3c331d3eb00446b9adfd311719c619
cp <877734642@qq.com>
2020-12-08 19:21:36 +0800

    +增加转正管理crud接口

---
+++ src/main/java/net/herdao/hdp/manpower/mpclient/service/MpStaffPositiveApprovalService.java
@@ -0,0 +1,45 @@
+/*
+ *    Copyright (c) 2018-2025, herdao All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * Neither the name of the herdao.net developer nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * Author: liang
+ */
+
+package net.herdao.hdp.manpower.mpclient.service;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.IService;
+import net.herdao.hdp.manpower.mpclient.entity.MpStaffPositiveApproval;
+
+/**
+ * 转正审批表
+ *
+ * @author cp
+ * @date 2020-12-08 11:02:30
+ */
+public interface MpStaffPositiveApprovalService extends IService<MpStaffPositiveApproval> {
+
+    /**
+     * 分页查询
+     * @return
+     */
+    IPage<MpStaffPositiveApproval> getPageInfo(Page<MpStaffPositiveApproval> page, MpStaffPositiveApproval mpStaffPositiveApproval);
+
+
+
+    /**
+     * 通过id删除转正审批表
+     */
+    void deleteById(String[] inputIds);
+}
