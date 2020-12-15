b8863bbb7dc885b4620646b6ec9200c479001dc1
liuxm <121786@gdpr.com>
2020-12-15 08:51:11 +0800

    更新流程调用接口

--- src/main/java/net/herdao/hdp/manpower/mpclient/service/impl/RecruitmentServiceImpl.java
+++ src/main/java/net/herdao/hdp/manpower/mpclient/service/impl/RecruitmentServiceImpl.java
@@ -438,7 +438,7 @@ public class RecruitmentServiceImpl extends ServiceImpl<RecruitmentMapper, Recru
     	String contentFK = recordId;
     	String flowContent = flowType;
     	
-    	return remoteWorkflowService.generateWorkflow(parameterJson, contentUrl, contentFK, flowContent);
+    	return remoteWorkflowService.generateWorkflow(parameterJson, contentUrl, contentFK, flowContent, SecurityConstants.FROM_IN);
     	
     }
     
