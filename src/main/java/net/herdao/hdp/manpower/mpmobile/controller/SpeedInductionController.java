373d11a6be3073b0ed32521bbe74428b34d7878e
cp <877734642@qq.com>
2020-12-17 15:14:55 +0800

    +移动端极速入职_增加swagger注释

--- src/main/java/net/herdao/hdp/manpower/mpmobile/controller/SpeedInductionController.java
+++ src/main/java/net/herdao/hdp/manpower/mpmobile/controller/SpeedInductionController.java
@@ -1,6 +1,8 @@
 package net.herdao.hdp.manpower.mpmobile.controller;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import net.herdao.hdp.common.core.util.R;
@@ -56,6 +58,10 @@ public class SpeedInductionController {
      * @return R*/
     @ApiOperation(value = "通过业务表ID查询(例如：人才表的主键ID)", notes = "通过业务表ID查询(例如：人才表的主键ID) ")
     @GetMapping("/attach")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id", value = "业务表ID(例如：人才表的主键ID)", required = true),
+            @ApiImplicitParam(name = "moduleTyp", value = "字典类型(文件所属字典类型 例如: 体检报告:MEDICAL_REPORT )", required = true)
+    })
 //    @PreAuthorize("@pms.hasPermission('generator_cardinformation_view')" )
     public R getAttachById( Long id, String moduleTyp) {
         return R.ok(attachFileService.getAttachFileById(id,moduleTyp));
@@ -72,6 +78,9 @@ public class SpeedInductionController {
      * */
     @ApiOperation(value = "通过业务表ID查询(例如：人才表的主键ID) ", notes = "通过业务表ID查询(例如：人才表的主键ID) ")
     @GetMapping("/payCard/{id}")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id", value = "业务表ID(例如：人才表的主键ID)", required = true)
+    })
 //    @PreAuthorize("@pms.hasPermission('generator_paycardinformation_view')")
     public R getCardById(@PathVariable("id") Long id) {
         return R.ok(payCardInformationService.getCardById(id));
