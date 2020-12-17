366a710f88609a1b5b0bc200e7d71e5fbc2dea50
liuchang <15735926905@163.com>
2020-12-17 19:16:00 +0800

    还原别人代码

--- src/main/java/net/herdao/hdp/manpower/mpclient/service/impl/RecruitmentServiceImpl.java
+++ src/main/java/net/herdao/hdp/manpower/mpclient/service/impl/RecruitmentServiceImpl.java
@@ -427,8 +427,8 @@ public class RecruitmentServiceImpl extends ServiceImpl<RecruitmentMapper, Recru
     	}
     	String contentFK = recordId;
     	String flowContent = flowType;
-//    	return remoteWorkflowService.generateWorkflow(parameterJson, contentUrl, contentFK, flowContent);
-    	return null;
+    	return remoteWorkflowService.generateWorkflow(parameterJson, contentUrl, contentFK, flowContent);
+
     }
 
     @Override
