20ac0a7bdc10eb0cea47819bdd43208928ad91b5
liuchang <15735926905@163.com>
2020-12-19 15:20:10 +0800

    重新定义详情返回值

--- src/main/java/net/herdao/hdp/manpower/mpclient/vo/staff/transfer/StaffTransferInfoVO.java
+++ src/main/java/net/herdao/hdp/manpower/mpclient/vo/staff/transfer/StaffTransferInfoVO.java
@@ -40,11 +40,14 @@ public class StaffTransferInfoVO {
     @ApiModelProperty(value = "调动前部门名称", name = "nowOrgName", example = "部门 A")
     private String nowOrgName;
 
-    @ApiModelProperty(value = "调动前岗位 id", name = "nowPostId", example = "2345")
-    private Long nowPostId;
+    /**
+     * 页面上岗位对应的是 mp_post_org（组织岗位关系表）的语义
+     */
+    @ApiModelProperty(value = "调动前岗位 id", name = "nowPosOrgId", example = "2345")
+    private Long nowPosOrgId;
 
-    @ApiModelProperty(value = "调动前岗位名称", name = "nowOrgName", example = "岗位 a")
-    private String nowPostName;
+    @ApiModelProperty(value = "调动前岗位名称", name = "nowPosOrgName", example = "岗位 a")
+    private String nowPosOrgName;
 
     @ApiModelProperty(value = "原职级 id", name = "jobLevelId", example = "123")
     private Long nowJobLevelId;
@@ -58,14 +61,17 @@ public class StaffTransferInfoVO {
     @ApiModelProperty(value = "调动后部门名称", name = "transOrgName", example = "部门 ")
     private String transOrgName;
 
-    @ApiModelProperty(value = "调动后岗位 id", name = "transPostId", example = "5432")
-    private Long transPostId;
+    @ApiModelProperty(value = "调动后岗位 id", name = "transPosOrgId", example = "5432")
+    private Long transPosOrgId;
 
-    @ApiModelProperty(value = "调动后岗位名称", name = "transPostName", example = "岗位 b")
-    private String transPostName;
+    @ApiModelProperty(value = "调动后岗位名称", name = "transPostOrgName", example = "岗位 b")
+    private String transPostOrgName;
 
-    @ApiModelProperty(value = "调动后职级", name = "transJobLevel", example = "123")
-    private Dictionary transJobLevel;
+    @ApiModelProperty(value = "调动后职级Id", name = "transJobLevelId", example = "123")
+    private Long transJobLevelId;
+
+    @ApiModelProperty(value = "调动后职级名称", name = "transJobLevelName")
+    private String transJobLevelName;
 
     @ApiModelProperty(value = "生效日期", name = "transStartDate", example = "20200901")
     private Long transStartDate;
@@ -94,27 +100,27 @@ public class StaffTransferInfoVO {
     @ApiModelProperty(value = "合生珠江系工龄", name = "threeGroupSeniority")
     private Double threeGroupSeniority;
 
-    @ApiModelProperty(value = "工资发放单位", name = "payUnit")
-    private Dictionary payUnit;
+    @ApiModelProperty(value = "工资发放单位 id", name = "payUnitId")
+    private Long payUnitId;
 
-    @ApiModelProperty(value = "公积金发放单位", name = "payUnit")
-    private Dictionary fundUnit;
+    @ApiModelProperty(value = "工资发放单位名称", name = "payUnitName")
+    private String payUnitName;
 
-    @ApiModelProperty(value = "社保购买单位", name = "securityUnit")
-    private Dictionary securityUnit;
+    @ApiModelProperty(value = "公积金发放单位 id", name = "fundUnitId")
+    private Long fundUnitId;
+
+    @ApiModelProperty(value = "公积金发放单位名称", name = "fundUnitName")
+    private String fundUnitName;
+
+    @ApiModelProperty(value = "社保购买id", name = "securityUnitId")
+    private Long securityUnitId;
+
+    @ApiModelProperty(value = "社保购买名称", name = "securityUnitName")
+    private String securityUnitName;
 
     @ApiModelProperty(value = "经办人意见", name = "remark")
     private String remark;
 
     @ApiModelProperty(value = "附件列表 id", name = "appendixIds")
-    private List<String> appendixIds;
-
-    @Data
-    // 字典
-    public static class Dictionary {
-        // 字符串名称
-        private String label;
-        // id
-        private Long value;
-    }
+    private String appendixIds;
 }
