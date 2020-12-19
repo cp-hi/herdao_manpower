2d5c0c26a51c0ae1dc02e92077ea370551c26b60
cp <877734642@qq.com>
2020-12-19 17:02:07 +0800

    +解决合并冲突

--- src/main/java/net/herdao/hdp/manpower/mpclient/service/impl/AttachFileServiceImpl.java
+++ src/main/java/net/herdao/hdp/manpower/mpclient/service/impl/AttachFileServiceImpl.java
@@ -22,6 +22,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
