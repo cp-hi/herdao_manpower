5f3a524e5ad90c0fb2f28c12d3b4f44bb28d49e7
ehr <378043672@qq.com>
2020-12-23 17:05:58 +0800

    修复职级删除未提示下级存在bug

--- src/main/java/net/herdao/hdp/manpower/mpclient/service/JobGradeService.java
+++ src/main/java/net/herdao/hdp/manpower/mpclient/service/JobGradeService.java
@@ -3,7 +3,10 @@ package net.herdao.hdp.manpower.mpclient.service;
 import net.herdao.hdp.manpower.mpclient.vo.jobLevel.JobGradeShortVO;
 import net.herdao.hdp.manpower.mpclient.entity.JobGrade;
 
+import java.io.Serializable;
 import java.util.List;
+
+import com.baomidou.mybatisplus.extension.toolkit.SqlHelper;
 
 /**
  * @author ljan
@@ -11,4 +14,6 @@ import java.util.List;
 public interface JobGradeService extends  EntityService<JobGrade> {
 
     List<JobGradeShortVO> jobGradeList(Long groupId);
+    
+    boolean removeById(Long id);
 }
