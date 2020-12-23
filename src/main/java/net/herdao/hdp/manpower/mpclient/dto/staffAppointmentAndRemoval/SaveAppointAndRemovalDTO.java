d02c207b15e82b73881682183922a4b05c4a0b62
liuchang <15735926905@163.com>
2020-12-23 19:05:09 +0800

    兼职任免字段定义

---
+++ src/main/java/net/herdao/hdp/manpower/mpclient/dto/staffAppointmentAndRemoval/SaveAppointAndRemovalDTO.java
@@ -0,0 +1,24 @@
+/*
+ * All rights Reserved, Designed By HerDao
+ * Copyright:    Copyright(C) 2020-2099
+ * Company:      HerDao Ltd.
+ */
+
+package net.herdao.hdp.manpower.mpclient.dto.staffAppointmentAndRemoval;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * @Author Liu Chang
+ * @Date 2020/12/23 2:34 下午
+ */
+@Data
+public class SaveAppointAndRemovalDTO {
+
+    @ApiModelProperty(value = "用户id", name = "userId")
+    private Long userId;
+
+    @ApiModelProperty(value = "经办人意见", name = "remark")
+    private String remark;
+}
